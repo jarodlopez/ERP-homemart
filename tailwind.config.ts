fa83ffb6b243e4a52bbedec1830b6889efbf2a9b
jarodlopez <127226881+jarodlopez@users.noreply.github.com>
2025-12-27 22:19:05 -0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,16 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       colors: {
-        primary: "#2563EB", // Azul estándar
+        primary: "#2563EB",
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'), // Lo incluimos en package.json, hay que activarlo
-  ],
+  plugins: [],
 };
 export default config;
+
